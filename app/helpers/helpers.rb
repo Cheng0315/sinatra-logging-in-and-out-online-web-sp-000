dc16acb560d9d303afda9dec44a9281a084f5fab
Cheng Vue <cvu3@ucsc.edu>
2019-01-07 05:06:16 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  
+  def self.current_user(user_session)
+    @current_user = User.find(user_session[:user_id])
+  end
+
+  def self.is_logged_in?(user_session)
+    !!!user_session.empty?
+  end
 end
