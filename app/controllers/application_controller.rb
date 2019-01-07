a593307fe0a494277dacac1b86576ec0ab882b46
Cheng Vue <cvu3@ucsc.edu>
2019-01-07 07:02:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
-      erb :account
-    elsif
+    if !Helpers.is_logged_in?(session)
       erb :error
+
+    elsif
+      erb :account
     end
   end
 
