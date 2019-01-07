1703f510c8ebd6b883c348bf790a731cf6e8642b
Cheng Vue <cvu3@ucsc.edu>
2019-01-07 05:16:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
-    Helper.
+    session[:user_id] = @user.id
+    redirect '/account'
   end
 
   get '/account' do
